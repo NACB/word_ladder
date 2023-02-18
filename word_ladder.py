2660a9037dbeb45931966b98ca1a3477c4da1391
Nathanael Baumann <nbaumann24@cmc.edu>
2023-02-17 18:07:54 -0800

    fixed dictionary iteration to stop "skipping"

--- word_ladder.py
+++ word_ladder.py
@@ -63,6 +63,7 @@ def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
                 dictionary.remove(word)
     return None
 
+
 def verify_word_ladder(ladder):
     '''
     Returns True if each entry of the input list is adjacent to its neighbors;
